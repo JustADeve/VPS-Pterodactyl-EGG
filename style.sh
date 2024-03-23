de9ae254b3103613d18de3b0b5e74e3f47f00411
JustADeve <142085654+JustADeve@users.noreply.github.com>
2024-03-23 19:57:55 +0100

    Update style.sh

--- style.sh
+++ style.sh
@@ -19,7 +19,12 @@ echo -e "ㅤ"
 echo -e "ㅤ"
 
 echo -e "${YELLOW} - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
-echo -e "${BOLD}${GREEN}Powered By"
+echo -e "${BOLD}${GREEN}Powered By: Lofycloud"
 echo -e "${RED}Egg provided by : Apex.cloud©️ Company 🌟"
 echo "VPS motorcyles._ egg made by: czo (Jake)"
+echo -e "${RED}Please run this before starting using your vps. :
+curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+Then: apt install python3 -y After this:
+chmod +x /usr/bin/systemctl  And you are ready!
+"
 echo -e "${YELLOW} - - - - - - - - - - - - - - - - ${WHITE}[ ${BOLD}${GREEN}TYPE YOUR COMMAND ${WHITE}]${YELLOW} - - - - - - - - - - - - - - "
