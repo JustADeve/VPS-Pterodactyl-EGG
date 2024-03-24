93da9290941faca3f37c41c841a35b9ef56a05e7
JustADeve <142085654+JustADeve@users.noreply.github.com>
2024-03-24 08:09:59 +0100

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -30,9 +30,9 @@ fi
 if [ ! -e "$ROOTFS_DIR/.installed" ]; then
 echo "#######################################################################################"
 echo "#"
-echo "#                                    > ApexCloud VPS <"
+echo "#                                    > Lofycloud <"
 echo "#"
-echo "#                           Copyright (C) 2022 - 2023, Apex.Cloud"
+echo "#                           Copyright (C) 2022 - 2023, Apex"
 echo "#"
 echo "#"
 echo "#######################################################################################"
@@ -61,7 +61,7 @@ echo "##########################################################################
       wget -O $ROOTFS_DIR/home/container/.bashrc \
       "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
       wget -O $ROOTFS_DIR/home/container/style.sh \
-      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
+      "https://raw.githubusercontent.com/JustADeve/VPS-Pterodactyl-EGG/main/style.sh"
       ;;
 
     1)
@@ -75,7 +75,7 @@ echo "##########################################################################
       wget -O $ROOTFS_DIR/home/container/.bashrc \
       "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
       wget -O $ROOTFS_DIR/home/container/style.sh \
-      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
+      "https://raw.githubusercontent.com/JustADeve/VPS-Pterodactyl-EGG/main/style.sh"
       ;;
 
     2)
@@ -89,7 +89,7 @@ echo "##########################################################################
       wget -O $ROOTFS_DIR/home/container/.bashrc \
       "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
       wget -O $ROOTFS_DIR/home/container/style.sh \
-      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
+      "https://raw.githubusercontent.com/JustADeve/VPS-Pterodactyl-EGG/main/style.sh"
       ;;
 
     *)
